ebaff6e40533ffa447cfddda2ea877345c179baa
Saurabh Badkas <scbadkas19@gmail.com>
2025-10-04 00:06:43 -0400

    Favicon and Sitemap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <Script
           id="ga-loader"
           strategy="afterInteractive"
