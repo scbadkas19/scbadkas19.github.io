9efbd2b081126f330b7cb8b03df207ea243817b0
Saurabh Badkas <scbadkas19@gmail.com>
2026-02-15 22:12:14 -0500

    Update portfolio

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -3,7 +3,7 @@ export const projects = [
     title: "Streaming Analytics Platform",
     description: "Architected a robust GCP data pipeline processing over 100,000 viewing events with a 40% reduction in processing time, and designed advanced Google Data Studio dashboards that boosted strategic decision-making efficiency by 25%.",
     image: "/images/projects/Netflix.png",
-    githubLink: "https://github.com/asJ26/Netflix-Data-Pipeline-and-Dashboard.git",
+    githubLink: "https://github.com/scbadkas19",
     skills: ["BigQuery","Google Cloud Platform (GCP)","Dataflow","Looker Studio","Python"]
   },
  
@@ -11,35 +11,35 @@ export const projects = [
     title: "IoT Sensor Data Pipeline",
     description: "Establishes a real-time data analytics pipeline using AWS, simulating IoT sensor data generation, processing it through AWS Lambda, storing in Amazon S3, and visualizing via Amazon QuickSight for dynamic insights.",
     image: "/images/projects/IOT_pipeline.png",
-    githubLink: "https://github.com/asJ26/IoT-sensor-Data-Pipeline.git",
+    githubLink: "https://github.com/scbadkas19",
     skills: ["AWS Kinesis", "AWS Lambda", "S3", "QuickSight", "CLI"]
   },
    {
     title: "Sentiment Analytics",
     description: "Developed a real-time railway tweet analysis platform using Python, Django, and Azure services to process social media data for sentiment analysis and emergency detection, enhancing railway operations.",
     image: "/images/projects/twitter.png",
-    githubLink: "https://github.com/asJ26/Twitter-pipeline-AZURE.git",
+    githubLink: "https://github.com/scbadkas19",
     skills: ["Azure", "Django", "PostgreSQL", "Python"]
   },
   {
     title: "Syracuse DBMS",
     description: "A comprehensive hospital management system, leveraging a relational database to manage extensive data on patients, medical history, physicians, appointments, procedures, and more, aiming to streamline hospital operations and patient care.",
     image: "/images/projects/DBMS.jpg",
-    githubLink: "https://github.com/asJ26/Syracuse-Dbms-project.git",
+    githubLink: "https://github.com/scbadkas19",
     skills: ["SQL", "MySQL server", "Python"]
   },
   {
     title: "Song Popularity Prediction",
     description: "Leverages Decision Tree and KNN algorithms to predict song popularity based on metrics like 'artist popularity', 'followers', and song characteristics, aiming to provide the music industry with actionable insights for strategic planning.",
     image: "/images/projects/Spotify.png",
-    githubLink: "https://github.com/asJ26/Spotify-Song-Popularity.git",
+    githubLink: "https://github.com/scbadkas19",
     skills: ["Spotify API", "Python", "SKlearn", "Pandas & NumPy", "Matplotlib"]
   },
   {
     title: "Airline Departure Timing Prediction",
     description: "Developed a data science project that analyzes airline departure timings to identify patterns and key factors influencing flight delays, providing valuable insights for airlines and passengers.",
     image: "/images/projects/MIT_Airline-Safety-01_0.jpg",
-    githubLink: "https://github.com/asJ26/Airline-Departure-Timing.git",
+    githubLink: "https://github.com/scbadkas19",
     skills: ["Pandas", "Scikit-learn", "Seaborn", "CSV"]
   },
  
